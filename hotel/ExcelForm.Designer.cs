aa4470b44df015b7b319289477dafeb501cfc049
abir_zennir <abirzennir3@gmail.com>
2025-01-02 22:32:24 +0100

    Modification de hotel

--- hotel/ExcelForm.Designer.cs
+++ hotel/ExcelForm.Designer.cs
@@ -38,6 +38,7 @@
             this.button_dashbord = new System.Windows.Forms.Button();
             this.label8 = new System.Windows.Forms.Label();
             this.label_exit = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -50,7 +51,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(1077, 324);
+            this.dataGridView1.Size = new System.Drawing.Size(764, 324);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
@@ -60,7 +61,7 @@
             this.button1.Font = new System.Drawing.Font("Showcard Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.InfoText;
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(903, 0);
+            this.button1.Location = new System.Drawing.Point(597, 0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(198, 125);
             this.button1.TabIndex = 1;
@@ -77,14 +78,14 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1101, 100);
+            this.panel1.Size = new System.Drawing.Size(795, 100);
             this.panel1.TabIndex = 2;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Snap ITC", 28F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(252, 9);
+            this.label6.Location = new System.Drawing.Point(61, 9);
             this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(451, 73);
@@ -94,13 +95,14 @@
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.button2);
             this.panel2.Controls.Add(this.button_dashbord);
             this.panel2.Controls.Add(this.button_logout);
             this.panel2.Controls.Add(this.button1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel2.Location = new System.Drawing.Point(0, 486);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1101, 125);
+            this.panel2.Size = new System.Drawing.Size(795, 125);
             this.panel2.TabIndex = 3;
             // 
             // button_logout
@@ -129,10 +131,10 @@
             this.button_dashbord.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button_dashbord.ForeColor = System.Drawing.SystemColors.Info;
             this.button_dashbord.Image = ((System.Drawing.Image)(resources.GetObject("button_dashbord.Image")));
-            this.button_dashbord.Location = new System.Drawing.Point(713, 0);
+            this.button_dashbord.Location = new System.Drawing.Point(397, 0);
             this.button_dashbord.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.button_dashbord.Name = "button_dashbord";
-            this.button_dashbord.Size = new System.Drawing.Size(190, 125);
+            this.button_dashbord.Size = new System.Drawing.Size(200, 125);
             this.button_dashbord.TabIndex = 9;
             this.button_dashbord.Text = "retour";
             this.button_dashbord.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -143,7 +145,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Monotype Corsiva", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(749, 64);
+            this.label8.Location = new System.Drawing.Point(559, 61);
             this.label8.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(191, 39);
@@ -154,7 +156,7 @@
             // 
             this.label_exit.AutoSize = true;
             this.label_exit.Font = new System.Drawing.Font("Snap ITC", 16F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_exit.Location = new System.Drawing.Point(1043, 9);
+            this.label_exit.Location = new System.Drawing.Point(730, 9);
             this.label_exit.Name = "label_exit";
             this.label_exit.Size = new System.Drawing.Size(46, 42);
             this.label_exit.TabIndex = 19;
@@ -162,12 +164,27 @@
             this.label_exit.TextAlign = System.Drawing.ContentAlignment.TopRight;
             this.label_exit.Click += new System.EventHandler(this.label_exit_Click);
             // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Info;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.Font = new System.Drawing.Font("Showcard Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(199, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 125);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "excel";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
             // ExcelForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Info;
-            this.ClientSize = new System.Drawing.Size(1101, 611);
+            this.ClientSize = new System.Drawing.Size(795, 611);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.dataGridView1);
@@ -194,5 +211,6 @@
         private System.Windows.Forms.Button button_dashbord;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label_exit;
+        private System.Windows.Forms.Button button2;
     }
 }
